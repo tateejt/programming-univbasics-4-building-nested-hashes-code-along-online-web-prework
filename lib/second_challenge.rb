cc8ac5310762541fb79aeb02c49443a2800c64cc
congenial-compressor-7742 <tateejt@gmail.com>
2019-09-30 04:06:28 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -1,13 +1,17 @@
 def second_challenge
   epic_tragedy = {
     :montague => {
-    :patriarch{},
-    :matriarch{},
-    :hero{},
-    :hero_friend []
+    :patriarch => {},
+    :matriarch => {},
+    :hero => {},
+    :hero_friend => []
 #your key/value pairs here!
       }, 
     :capulet => {
+      :patriarch =>{}
+      :matriarch =>{}
+      :hero =>{}
+      :hero_friend => []
       #your key/value pairs here!
       }}
 
