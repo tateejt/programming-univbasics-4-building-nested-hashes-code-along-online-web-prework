8ed507806a48411b3849ba5d13986ee32187b829
congenial-compressor-7742 <tateejt@gmail.com>
2019-09-30 04:11:29 +0000

    Done.

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -1,9 +1,17 @@
 def second_challenge
   epic_tragedy = {
     :montague => {
-      #your key/value pairs here!
+    :patriarch => {},
+    :matriarch => {},
+    :hero => {},
+    :hero_friend => []
+#your key/value pairs here!
       }, 
     :capulet => {
+      :patriarch =>{}
+      :matriarch =>{}
+      :hero =>{}
+      :hero_friend => []
       #your key/value pairs here!
       }}
 
--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -1,13 +1,13 @@
 def third_challenge
   epic_tragedy = {
    :montague => {
-      :patriarch => {
+      :patriarch => { :name => "Lord Montague", :age => "53"
         #your key/value pairs here
         },
-      :matriarch => {
+      :matriarch => {:name => "Lady Montague", :age => "54"
         #your key/value pairs here
         },
-      :hero => {
+      :hero => {:name => "Romeo", :age "15", :status
         #your key/value pairs here
         },
       :hero_friends => []
